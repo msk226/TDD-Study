b202e0af0633694c1a727a4500b8732ba6f90f47
msk226 <kminseok5167@gmail.com>
2025-07-17 14:51:59 +0900

    test: 납부일과 한달 뒤 일자가 같지 않은 경우에 대한 테스트 코드 작성

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -10,10 +10,15 @@ public class ExpiryDateCalculatorTest {
 
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨() {
-        LocalDate billingDate = LocalDate.of(2025, 3, 1);
-
         assertExpiryDate(LocalDate.of(2025, 4, 1), 10_000, LocalDate.of(2025, 5, 1));
         assertExpiryDate(LocalDate.of(2025, 5, 5), 10_000, LocalDate.of(2025,6, 5));
+    }
+
+    @Test
+    void 납부일과_한달_뒤_일자가_같지_않음() {
+        assertExpiryDate(LocalDate.of(2025, 1, 31), 10_000, LocalDate.of(2025, 2, 28));
+        assertExpiryDate(LocalDate.of(2025, 5, 31), 10_000, LocalDate.of(2025, 6, 30));
+        assertExpiryDate(LocalDate.of(2020, 1, 31), 10_000, LocalDate.of(2020, 2, 29));
     }
 
     private void assertExpiryDate(
