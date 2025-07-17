5b3020ad81983c5cc2fda4dc2dbece93f687ab81
msk226 <kminseok5167@gmail.com>
2025-07-17 14:50:09 +0900

    refactor: 중복 제거를 위한 메서드 추출

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -1,5 +1,7 @@
 package chap03;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,19 +11,17 @@ public class ExpiryDateCalculatorTest {
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨() {
         LocalDate billingDate = LocalDate.of(2025, 3, 1);
-        int payAmount = 10_000;
 
+        assertExpiryDate(LocalDate.of(2025, 4, 1), 10_000, LocalDate.of(2025, 5, 1));
+        assertExpiryDate(LocalDate.of(2025, 5, 5), 10_000, LocalDate.of(2025,6, 5));
+    }
+
+    private void assertExpiryDate(
+            LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate
+    ) {
         ExpiryDateCalculator cal = new ExpiryDateCalculator();
         LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
-
-        Assertions.assertEquals(LocalDate.of(2025, 4, 1), expiryDate);
-
-        LocalDate billingDate2 = LocalDate.of(2025, 5, 5);
-        int payAmount2 = 10_000;
-
-        LocalDate expiryDate2 = cal.calculateExpiryDate(billingDate2, payAmount2);
-
-        Assertions.assertEquals(LocalDate.of(2025, 6, 5), expiryDate2);
+        assertEquals(expectedExpiryDate, expiryDate);
     }
 
 }
