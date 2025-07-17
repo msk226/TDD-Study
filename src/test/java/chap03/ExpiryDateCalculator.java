0eda480555e5c0c48d27779ea00ea7fe2507266d
msk226 <kminseok5167@gmail.com>
2025-07-17 15:09:50 +0900

    refactor: 상수를 변수로 변경

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -4,15 +4,17 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
 
+    private static final int ADDED_MONTH = 1;
+
     public LocalDate calculateExpiryDate(PaymentData paymentData) {
         if (paymentData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = paymentData.getBillingDate().plusMonths(1);
+            LocalDate candidateExp = paymentData.getBillingDate().plusMonths(ADDED_MONTH);
             if (paymentData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
                 return candidateExp.withDayOfMonth(
                         paymentData.getFirstBillingDate().getDayOfMonth()
                 );
             }
         }
-        return paymentData.getBillingDate().plusMonths(1);
+        return paymentData.getBillingDate().plusMonths(ADDED_MONTH);
     }
 }
