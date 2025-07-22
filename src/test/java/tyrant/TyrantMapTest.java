74732fd8f7973cef34d77c8574321a6624dc2db5
msk226 <kminseok5167@gmail.com>
2025-07-22 22:02:19 +0900

    refactor: 디테일 코드를 객체로 추출

--- src/test/java/tyrant/TyrantMapTest.java
+++ src/test/java/tyrant/TyrantMapTest.java
@@ -11,24 +11,32 @@ public class TyrantMapTest {
 
     @Test
     void nothing() throws IOException {
-        Socket socket = new Socket("localhost", 1978);
-        OutputStream writer = socket.getOutputStream();
-        writer.write(0xC8);
-        writer.write(0x10);
-        writer.write(0);
-        writer.write(0);
-        writer.write(0);
-        writer.write(3);
-
-        writer.write(0);
-        writer.write(0);
-        writer.write(0);
-        writer.write(5);
-        writer.write(new byte[]{'k', 'e', 'y'});
-        writer.write(new byte[]{'v', 'a', 'l', 'u', 'e'});
-
-        InputStream reader = socket.getInputStream();
-        int status = reader.read();
-        Assertions.assertEquals(0, status);
+        TyrantMap tyrantMap = new TyrantMap();
+        tyrantMap.invoke();
     }
+
+    private class TyrantMap {
+        public void invoke() throws IOException {
+            Socket socket = new Socket("localhost", 1978);
+            OutputStream writer = socket.getOutputStream();
+            writer.write(0xC8);
+            writer.write(0x10);
+            writer.write(0);
+            writer.write(0);
+            writer.write(0);
+            writer.write(3);
+
+            writer.write(0);
+            writer.write(0);
+            writer.write(0);
+            writer.write(5);
+            writer.write(new byte[]{'k', 'e', 'y'});
+            writer.write(new byte[]{'v', 'a', 'l', 'u', 'e'});
+
+            InputStream reader = socket.getInputStream();
+            int status = reader.read();
+            Assertions.assertEquals(0, status);
+        }
+    }
+
 }
