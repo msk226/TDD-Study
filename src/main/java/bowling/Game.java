5c366bb600c08f5877c9c17e199998ba24d30c6d
msk226 <kminseok5167@gmail.com>
2025-07-22 22:42:55 +0900

    blue: isSpare() 메서드 추출

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -11,17 +11,21 @@ public class Game {
 
     public int getScore() {
         int score = 0;
-        int i = 0;
+        int firstFrame = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (rolls[i] + rolls[i + 1] == 10) {
-                score += 10 + rolls[i + 2];
-                i += 2;
+            if (isSpare(firstFrame)) {
+                score += 10 + rolls[firstFrame + 2];
+                firstFrame += 2;
             } else {
-                score += rolls[i] + rolls[i + 1];
-                i += 2;
+                score += rolls[firstFrame] + rolls[firstFrame + 1];
+                firstFrame += 2;
             }
         }
 
         return score;
+    }
+
+    private boolean isSpare(int firstFrame) {
+        return rolls[firstFrame] + rolls[firstFrame + 1] == 10;
     }
 }
