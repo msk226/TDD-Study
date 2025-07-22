d4db2eee94b770ec1d4015a6d85677b38baed77e
msk226 <kminseok5167@gmail.com>
2025-07-22 22:10:11 +0900

    refactor: Socket 연결과 관련된 코드 메서드 추출

--- src/test/java/tyrant/TyrantMapTest.java
+++ src/test/java/tyrant/TyrantMapTest.java
@@ -1,5 +1,7 @@
 package tyrant;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -12,6 +14,15 @@ import org.junit.jupiter.api.Test;
 public class TyrantMapTest {
 
     @Test
+    public void get_retrives_what_was_put() {
+//        TyrantMap map = new TyrantMap(); // 3.1
+//        final byte[] key = "key".getBytes(); // 3.2
+//        final byte[] value = "value".getBytes(); // 3.3
+//        map.put(key, value); // 2
+//        assertEquals(value, map.get(key)); // 1
+    }
+
+    @Test
     void nothing() throws IOException {
         TyrantMap tyrantMap = new TyrantMap();
         tyrantMap.put(new byte[]{'k', 'e', 'y'}, new byte[]{'v', 'a', 'l', 'u', 'e'});
@@ -21,9 +32,13 @@ public class TyrantMapTest {
         public static final int OPERATION_PREFIX = 0xC8;
         public static final int PUT_OPERATION = 0x10;
 
+        private Socket socket;
+        private DataOutputStream writer;
+        private DataInputStream reader;
+
         public void put(byte[] key, byte[] value) throws IOException {
-            Socket socket = new Socket("localhost", 1978);
-            DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
+            open();
+
             writer.write(OPERATION_PREFIX);
             writer.write(PUT_OPERATION);
             writer.writeInt(key.length);
@@ -31,9 +46,14 @@ public class TyrantMapTest {
             writer.write(key);
             writer.write(value);
 
-            DataInputStream reader = new DataInputStream(socket.getInputStream());
             int status = reader.read();
-            Assertions.assertEquals(0, status);
+            assertEquals(0, status);
+        }
+
+        private void open() throws IOException {
+            socket = new Socket("localhost", 1978);
+            writer = new DataOutputStream(socket.getOutputStream());
+            reader = new DataInputStream(socket.getInputStream());
         }
 
     }
